01186de247676ca27876fc3f065aa2fbad6c4087
oysteinbakken <oystein.bakken@nav.no>
2022-12-29 22:20:51 +0100

    Hello another World

--- svcs/Main.kt
+++ svcs/Main.kt
@@ -1,7 +1,7 @@
 package svcs
 
 fun main(args: Array<String>) {
-    println("Hello this World!")
+    println("Hello another World!")
     if (args.isEmpty() || args[0] == "--help") {
         println(getHelpPage())
     } else {
