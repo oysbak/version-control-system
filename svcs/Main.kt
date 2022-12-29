ba287500b7ae650782c365f9a15f65fbcd13cc83
oysteinbakken <oystein.bakken@nav.no>
2022-12-29 21:20:40 +0100

    Stage 1/4: Help page

---
+++ svcs/Main.kt
@@ -0,0 +1,24 @@
+package svcs
+
+fun main(args: Array<String>) {
+    if (args.isEmpty() || args[0] == "--help") {
+        println(getHelpPage())
+    } else {
+        when (args[0]) {
+            "config" -> println("Get and set a username.")
+            "add" -> println("Add a file to the index.")
+            "log" -> println("Show commit logs.")
+            "commit" -> println("Save changes.")
+            "checkout" -> println("Restore a file.")
+            else -> println("'${args[0]}' is not a SVCS command.")
+        }
+    }
+}
+
+fun getHelpPage() =
+    "These are SVCS commands:\n" +
+            "config     Get and set a username.\n" +
+            "add        Add a file to the index.\n" +
+            "log        Show commit logs.\n" +
+            "commit     Save changes.\n" +
+            "checkout   Restore a file."
